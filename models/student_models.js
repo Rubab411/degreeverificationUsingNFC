2f3518895ee19b264545aecb303594567e4ee011
Rubab411 <rubabfatima9197@gmail.com>
2025-10-24 20:56:37 +0500

    update student_models, node_modules,package.json,package-lock.json

--- models/student_models.js
+++ models/student_models.js
@@ -1,26 +1,27 @@
-  const mongoose = require("mongoose");
-  const { v4: uuidv4 } = require("uuid");
+// student_models.js
+const mongoose = require("mongoose");
+const { v4: uuidv4 } = require("uuid"); // UUID v8 compatible
 
-  const studentSchema = new mongoose.Schema(
-    {
-      fullName: { type: String, required: true },
-      fatherName: { type: String },
-      dob: { type: String },
-      gender: { type: String },
-      roll: { type: String, required: true, unique: true },
-      program: { type: String },
-      department: { type: String },
-      batch: { type: String },
-      campus: { type: String },
-      startYear: { type: String },
-      endYear: { type: String },
-      currentSemester: { type: String },
-      cgpa: { type: String },
-      uid: { type: String, default: uuidv4, unique: true },
-      nfcUID: { type: String, default: null }, // Actual chip UID
-     verifyURL: { type: String, default: null }, // For mobile verification
-    },
-    { timestamps: true }
-  );
+const studentSchema = new mongoose.Schema(
+  {
+    fullName: { type: String, required: true },
+    fatherName: { type: String },
+    dob: { type: String },
+    gender: { type: String },
+    roll: { type: String, required: true, unique: true },
+    program: { type: String },
+    department: { type: String },
+    batch: { type: String },
+    campus: { type: String },
+    startYear: { type: String },
+    endYear: { type: String },
+    currentSemester: { type: String },
+    cgpa: { type: String },
+    uid: { type: String, default: uuidv4, unique: true }, // Auto UUID
+    nfcUID: { type: String, default: null }, // Actual chip UID
+    verifyURL: { type: String, default: null }, // For mobile verification
+  },
+  { timestamps: true }
+);
 
-  module.exports = mongoose.model("Student", studentSchema);
+module.exports = mongoose.model("Student", studentSchema);
