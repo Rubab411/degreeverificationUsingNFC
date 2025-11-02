dff5ba1c8769955405ffdb7d9d335a63253dc3e9
Rubab411 <rubabfatima9197@gmail.com>
2025-11-02 21:15:39 +0500

    changing in student-controller

--- controller/student-controller.js
+++ controller/student-controller.js
@@ -108,15 +108,16 @@ const sendOtp = async (req, res) => {
     await student.save();
 
     // ✅ Send OTP via email
-   const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com", // ✅ Gmail SMTP host
-  port: 465,              // ✅ Use 465 for SSL (secure)
-  secure: true,           // ✅ true for port 465, false for 587
+const transporter = nodemailer.createTransport({
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  secure: false, // Brevo STARTTLS use karta hai
   auth: {
-    user: process.env.EMAIL_USER, // Gmail address
-    pass: process.env.EMAIL_PASS, // App password (not Gmail password)
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
   },
 });
+
     await transporter.sendMail({
       from: "your_email@gmail.com",
       to: email,
